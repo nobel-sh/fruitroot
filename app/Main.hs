84d6b61dce2be2af632614e6d8de68b91e078543
nobel <nobel2073@gmail.com>
2024-02-23 23:25:47 +0545

    feat: add basic md parser

---
+++ app/Main.hs
@@ -0,0 +1,9 @@
+module Main (main) where
+
+import Parser
+import Markdown
+import MD
+import Lib
+
+main :: IO ()
+main = someFunc
---
+++ src/Parser.hs
@@ -0,0 +1,64 @@
+module MD where
+
+import Text.Parsec
+import Text.Parsec.String
+
+data Elem =
+        Paragraph Inlines
+        | Header Level Inlines
+        deriving (Eq, Show)
+
+data  Inline =
+        Emph Inlines
+        | Strong Inlines
+        | Literal String
+        deriving (Eq, Show)
+
+type Inlines = [Inline]
+
+type Document = [Elem]
+type Level = Int
+
+pInline :: Parser Inline
+pInline = choice[
+            try pStrong
+            , try pEmph
+            , pLiteral]
+
+pElem :: Parser Elem
+pElem = choice [
+          try pHeading
+          , pParagraph
+        ]
+
+pDocument = manyTill pElem eof
+
+pParagraph :: Parser Elem
+pParagraph = do
+  content <- manyTill pInline endOfLine
+  return $ Paragraph content
+
+pHeading :: Parser Elem
+pHeading = do
+  level <- length <$> many1 (char '#')
+  _ <- many1 space
+  content <- manyTill pInline endOfLine
+  return $ Header level content
+
+
+pLiteral :: Parser Inline
+pLiteral = do
+  lit <- manyTill anyChar (lookAhead (oneOf "\n*_"))
+  return $ Literal lit
+
+pStrong :: Parser Inline
+pStrong = do
+  _ <- string "**"
+  content <- manyTill pInline (string "**")
+  return $ Strong content
+
+pEmph :: Parser Inline
+pEmph = do
+  _ <- string "*"
+  content <- manyTill pInline (string "*")
+  return $ Emph content
